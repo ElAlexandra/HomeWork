58a8fd816cbd8caab63aa311d1d771d13b4409dd
ElAlexandra <85993355+ElAlexandra@users.noreply.github.com>
2021-06-28 18:31:06 +0300

    Add files via upload

--- ViewController.swift
+++ ViewController.swift
@@ -1,8 +1,8 @@
 //
 //  ViewController.swift
-//  Lesson 4. Task 3
+//  Task 4. Lesson 5
 //
-//  Created by Александра  Значёнок on 22.06.21.
+//  Created by Александра  Значёнок on 26.06.21.
 //
 
 import UIKit
@@ -11,106 +11,59 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        // Task 1
-        
-       
-            var tuple1 = (name:"Alex", age: 23, sallary: 1000.0)
-            var tuple2 = (name:"Vova", age: 35, sallary: 1500.0)
-            var tuple3 = (name:"Ivan", age: 78, sallary: 700.50)
         
         
-        func augmentSallary(name: String, age: Int, sallary: Float) {
-            
-            switch tuple1.age {
-               case 18...30:
-                augmentSallary(name: "Alex", age: 23, sallary: 1000.0 * 1.5)
-               default:
-                augmentSallary(name: "Alex", age: 23, sallary: 1000.0 * 3)
-
-            switch tuple2.age {
-               case 31...34:
-                augmentSallary(name: "Vova", age: 35, sallary: 1500.0 * 2)
-               default:
-                augmentSallary(name: "Vova", age: 35, sallary: 1500.0 * 3)
-
-            switch tuple3.age {
-                case 50...60:
-                    augmentSallary(name: "Ivan", age: 78, sallary: 700.50 / 5)
-                default:
-                    augmentSallary(name: "Ivan", age: 78, sallary: 700.50 * 3)
-                    
+    // Task 1
+        
+        let alphabet:String = "abcdefghijklmnopqrstuvwxyz"
+        let lettre: Character = "q"
+        
+        var position = 0
+         
+        for number in alphabet {
+            position += 1
+            if number == lettre {
+              print("The letter \(lettre) in the alphabet is \(position)")
             }
-                
-               
-                    
-               // вроде сделала проверку, создала функцию, но принт, почему то ничего не выводит, как вывести в принт 3 разных значения? я пробовала по разному, ничего не выводит
-//                    print(augmentSallary(name: "Alex", age: 23, sallary: 1000.0))
-//                    print(augmentSallary(name: "Vova", age: 35, sallary: 1500.0))
-//                    print(augmentSallary(name: "Ivan", age: 78, sallary: 700.50))
-                    
+        }
+       
         
-//                    это тоже не выводит(совершенно пустая консоль)
-//
-//                    print(tuple1)
-//                    print(tuple2)
-//                    print(tuple3)
-                }
-                
-            // Task 2
-            
         
-                let a: Int = 13
-                let b: Int = 24
-                let c: Int = 66
-                
-                func sumNumber() {
-                    let  sum = (a + b + c) / 3
-                    print(sum)
-                    }
-                sumNumber()
-                
-                // вроде сейчас правильно создала и вызвала фунцкцию, но консоль совершенно пустая
-                
-            
-            // Task 3
-            
-            
-            
-                var n: Int = 5000
-                var y: Int = 3
-                var p: Int = 12
-              
-                func creditSum() {
-                    var m: Int = (n * p * ((1 + p) * y) / (12 * ((1 + p) * y) - 1))
-                    var s: Int = (m * 12) * y
-                    
-                    creditSum()
-                    
-                    print("Сумма кредита - \(n)")
-                    print("Период - \(y)")
-                    print("Процент - \(p)")
-                    print("Ежемесячно - \(m)")
-                    print("Общая сумма - \(s)")
-                }
-            
-            // не могу проверить правильно ли сделала, ничего не выводит в консоль, она по прежнему пустая просто(наверное где то с самого начала неправильно сделала)
-            
-            
-            
+        // Task 2
+        
+        
+        let dayInMonth: [Int] = [31,28,31,30,31,30,31,31,30,31,30,31]
+    
+        for days in dayInMonth {
+          print("Number of days - \(days)")
         }
         
-       
+        let namesOfMonths: [String] = ["Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"]
         
-       
+        for month in 0..<namesOfMonths.count {
+            print("\(namesOfMonths[month]) = \(dayInMonth[month])")
+        }
+        
+        let tupleMonth = [("Январь", 31), ("Февраль", 28), ("Март", 31), ("Апрель", 30), ("Май", 31), ("Июнь", 30), ("Июль", 31), ("Август", 31), ("Сентябрь", 30), ("Октябрь", 31), ("Ноябрь", 30), ("Декабрь", 31)]
+        print(tupleMonth)
+        
+     
+        for value in tupleMonth.reversed()  {
+            print(value)
+        }
+        
+        
+        
         
             
         
-    
-
-
-}
-
+        
+        
+        
+       
     }
+
+
 }
+
+
